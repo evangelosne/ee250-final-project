c31956f531bdd747b3cb1ec57c88bf3e12d5daa4
Evangelos <neophyto@usc.edu>
2024-12-03 12:38:51 -0800

    jkjk

--- rpi_load.py
+++ rpi_load.py
@@ -4,9 +4,9 @@ import sys
 import time
 # By appending the folder of all the GrovePi libraries to the system path here,
 # we are successfully `import grovepi`
-sys.path.append('/grovepi/Software/Python')
+sys.path.append('/GrovePi-EE250/Software/Python')
 # # This append is to support importing the LCD library.
-sys.path.append('/grovepi/Software/Python/grove_rgb_lcd')
+sys.path.append('/GrovePi-EE250/Software/Python/grove_rgb_lcd')
 
 import grovepi
 from grove_rgb_lcd import setRGB, setText_norefresh, setText
