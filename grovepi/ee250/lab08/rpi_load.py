d28041a9e2dc5d187f2298d4bc73a0838de84a0a
Evangelos <neophyto@usc.edu>
2024-12-03 12:34:15 -0800

    fdfd

---
+++ grovepi/ee250/lab08/rpi_load.py
@@ -0,0 +1,23 @@
+#GROVEPI STUFF
+
+import sys
+import time
+# By appending the folder of all the GrovePi libraries to the system path here,
+# we are successfully `import grovepi`
+sys.path.append('/GrovePi-EE250/Software/Python')
+# # This append is to support importing the LCD library.
+sys.path.append('/GrovePi-EE250/Software/Python/grove_rgb_lcd')
+
+import grovepi
+from grove_rgb_lcd import setRGB, setText_norefresh, setText
+
+grovepi.set_bus("RPI_1")
+setText("")
+buf = ''
+for i in range(10):
+    buf = buf + '#'
+    setText_norefresh(buf)
+    setRGB(225,165,0)
+    setText_norefresh('\nLoading...')
+setRGB(135,202,100)
+setText_norefresh('\nComplete!')
