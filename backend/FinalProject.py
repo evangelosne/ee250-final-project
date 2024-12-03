aee0f24cb3ca7dbc8c3a7add7a957c02153e7ea5
Evangelos <neophyto@usc.edu>
2024-12-02 22:42:45 -0800

    grove uncommented

--- backend/FinalProject.py
+++ backend/FinalProject.py
@@ -4,14 +4,14 @@ import sys
 import time
 # By appending the folder of all the GrovePi libraries to the system path here,
 # we are successfully `import grovepi`
-# sys.path.append('/GrovePi-EE250/Software/Python')
+sys.path.append('/GrovePi-EE250/Software/Python')
 # # This append is to support importing the LCD library.
-# sys.path.append('/GrovePi-EE250/Software/Python/grove_rgb_lcd')
+sys.path.append('/GrovePi-EE250/Software/Python/grove_rgb_lcd')
 
-# import grovepi
-# from grove_rgb_lcd import setRGB, setText_norefresh, setText
+import grovepi
+from grove_rgb_lcd import setRGB, setText_norefresh, setText
 
-# grovepi.set_bus("RPI_1")
+grovepi.set_bus("RPI_1")
 
 #API STUFF
 import requests
@@ -60,15 +60,15 @@ def get_trending(querystring):
             if channel_name != 'N/A':
                 channel_names.append(channel_name)
 
-            # if len(channel_names)<=9:
-            #     buf = buf + '#'
-                # setText_norefresh(buf)
-                # setRGB(225,165,0)
-                # setText_norefresh('\nLoading...')
-            # elif len(channel_names)==10:
-            #     buf = buf + '##'
-                # setRGB(135,202,100)
-                # setText_norefresh('\nComplete!')
+            if len(channel_names)<=9:
+                buf = buf + '#'
+                setText_norefresh(buf)
+                setRGB(225,165,0)
+                setText_norefresh('\nLoading...')
+            elif len(channel_names)==10:
+                buf = buf + '##'
+                setRGB(135,202,100)
+                setText_norefresh('\nComplete!')
 
             print(f"""
             Title: {title}
